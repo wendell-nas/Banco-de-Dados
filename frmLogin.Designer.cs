ad221b78339d6a4f19fc77763d7224dfadfbec38
Wendell Nascimento Carneiro <128191009+wendell-nas@users.noreply.github.com>
2023-08-10 21:34:29 -0300

    Add files via upload

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CheckPecas2._0
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(98, 51);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(260, 26);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(98, 99);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(260, 26);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.White;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(283, 134);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 31);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(378, 190);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controle de acesso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ frmLogin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckPecas2._0
+{
+    public partial class frmLogin : Form
+    {   //INSERINDO O BANCO DE DADOS
+        string strConexao = @"Data Source=.\SQLEXPRESS;Initial Catalog=dbCheckPecas;User ID=sa;Password=sql2022"; //PEGO ESSE CODIGO NA PROPIEDADE DO BD
+        SqlConnection conexao; //INICIO UMA CONEXAO
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+
+            conexao = new SqlConnection(strConexao); //CONECTO MEU BANCO DE DADOS NA FUNCAO CONEXAO
+            SqlCommand cmd = new SqlCommand(); //CRIO UM COMANDO PARA REALIZAR ALGUMAS ATIVIDADES
+            cmd.Connection = conexao; //CONECTO MINHA CONEXAO
+            cmd.CommandText = "SELECT loginUsuario FROM tblUsuarios WHERE loginUsuario = @acesso"; //INSIRO UM CMD TEXT PARA FAZER A BUSCA DOS DADOS
+            cmd.Parameters.AddWithValue("@acesso", usuario); //CRIO UM CMD PARAMETROS PARA MUDAR A STRING
+            conexao.Open(); //ABRO MINHA CONEXAO
+
+            if (cmd.ExecuteScalar() == null) //VERIFICAO PRA VER SE TEM ALGUM DADO "ESSE CMD EXECUTE É PARA BUSCAR OS DADOS"
+            {
+                DialogResult resposta = MessageBox.Show("Usuário inexistente! " +
+                "Deseja realizar o cadastro?", "CADASTRO", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question); // CASO A BUSCA PELOS DADOS SEREM NULL 
+
+                if (resposta != DialogResult.Yes)
+                {
+                    cmd.CommandText = "INSERT INTO tblUsuarios VALUES(@login,@senha)"; //FACO ESSE CMD TEXT PARA INSERIR OS DADOS NO MEU BD
+                    cmd.Parameters.AddWithValue("@login", usuario);
+                    cmd.Parameters.AddWithValue("@senha", senha);
+                    cmd.ExecuteNonQuery(); // FACO ESSE CMD EXECUTE PARA ADICIONAR OS DADOS NO BD
+                    MessageBox.Show("Cadastro realizado com sucesso!");
+                }
+                else
+                {
+                    txtSenha.ResetText();
+                    txtUsuario.ResetText();
+                }
+            }
+            else
+            {
+                cmd.CommandText = "SELECT senhaUsuario FROM tblUsuarios WHERE loginUsuario = @user"; //USO ESSE CMD PARA VERIFICAR O USUARIO
+                cmd.Parameters.AddWithValue("@user", usuario);
+                if (senha != cmd.ExecuteScalar().ToString()) // USO ESSE IF PARA VERIFICAR SE A SENHA ESTA CORRETA 
+                {
+                    MessageBox.Show("A senha não confere!");
+                }
+
+            }
+            
+        }
+    }
+}
